64f44bc0def10a96ad50a56147d59338ec4ded43
1NetoDev <anderaes@gmail.com>
2025-04-28 01:13:50 -0300

    Site Finalizado

--- js/prints.js
+++ js/prints.js
@@ -2,22 +2,15 @@ document.addEventListener('DOMContentLoaded', function() {
     const slides = document.querySelectorAll('.carousel-slide-screenshots');
     let currentIndex = 0;
 
-    // Garante que pelo menos um slide esteja visível inicialmente
     if (slides.length > 0) {
         slides[0].classList.add('active');
     }
 
     function nextSlide() {
-        // Remove a classe 'active' do slide atual
         slides[currentIndex].classList.remove('active');
-
-        // Calcula o índice do próximo slide
         currentIndex = (currentIndex + 1) % slides.length;
-
-        // Adiciona a classe 'active' ao próximo slide
         slides[currentIndex].classList.add('active');
     }
 
-    // Define o intervalo para trocar os slides (3 segundos = 3000 milissegundos)
     setInterval(nextSlide, 3000);
 });
